7405a73a87d429687272a48019e8d5db81f994f7
oppara <oppara@oppara.tv>
2017-10-13 16:11:24 +0900

    Add fingers crossed

---
+++ try_fingers_crossed.php
@@ -0,0 +1,26 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Monolog\Logger;
+use Monolog\Handler\FingersCrossedHandler;
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Handler\FingersCrossed\ErrorLevelActivationStrategy;
+
+
+$path = __DIR__ . '/logs/mono.log';
+$logger = new Logger('basic');
+$logger->pushHandler(
+    new FingersCrossedHandler(
+        new RotatingFileHandler($path, 2, Logger::INFO),
+        new ErrorLevelActivationStrategy(Logger::ALERT)
+    )
+);
+
+$logger->debug('debug');
+$logger->info('info');
+$logger->notice('notice');
+$logger->warning('warning');
+$logger->error('error');
+$logger->critical('critical');
+$logger->alert('alert');
+$logger->emergency('emergency');
